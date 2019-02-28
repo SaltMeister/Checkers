2a4582ecb987b9b8d53f2fc8ffadba07c552e8bc
SaltMeister <huisimonliu@gmail.com>
2019-02-27 20:34:46 -0500

    got the code to work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,48 +3,73 @@ import '../css/App.css';
 import { Question } from './Question.jsx';
 import AnswerButton from './AnswerButton';
 import { app } from 'firebase';
-//import { buildFirebase} from '../clients/firebase.js';
-// import components
+import firebase from 'firebase';
 
-//var firebaseDatabase = buildFirebase();
- // firebaseDatabase.ref("question").once("value", (snapshot) => {
-  //  this.setState();
-   //   console.log(snapshot.val());
-//});
+export function getQuestions(cb) {
+  const app = firebase.initializeApp({
+    apiKey: "AIzaSyADAYC7lX5QVEspv8BUeV2uDqrFle8yQpk",
+    authDomain: "studio-trivia-db.firebaseapp.com",
+    databaseURL: "https://studio-trivia-db.firebaseio.com",
+    projectId: "studio-trivia-db",
+    storageBucket: "studio-trivia-db.appspot.com",
+    messagingSenderId: "736024037811"
+  });
+  firebase.database(app).ref('/questions').on('value', (snapshot) => {
+    let questions = Object.values(snapshot.val());
+    cb(questions);
+  });
+}
+
 class App extends Component {
   constructor (props) {
     super(props);
     this.state =   {  questionText: "What is the capital of New York",
-                      answer: "Albany",
+                      correct_choice_index: 2,
                       choices: ["Austin", "SF", "Albany", "New York City"],
-                      answerClicked: false,
+                      currentIndex: 0,
                    }
+      getQuestions((questions) => {
+      console.log(questions);
+        this.setState({
+          ...this.state,
+          "questions": questions
+        });
+      });               
   }
-
   handleClick(clickedAnswer)
   {
-      this.setState({
-          answerClicked: true,
-      })
-      if(this.state.choices[clickedAnswer] === this.state.answer) 
+      if(clickedAnswer === this.state.correct_choice_index) 
       {
         this.setState({
           choices: ["Correct", "Correct", "Correct", "Correct"],
         })
       }
-      else if (this.state.choices[clickedAnswer] !== this.state.answer)
+      else if (clickedAnswer !== this.state.correct_choice_index)
       {
         this.setState({
           choices: ["Incorrect", "Incorrect", "Incorrect", "Incorrect"],
         })
       }
+      
+      
+  }
 
+  resetButton()
+  {
+    this.setState({
+      questionText: this.state.questions[this.state.currentIndex].question_text,
+      questions: this.state.questions,
+      correct_choice_index: this.state.questions[this.state.currentIndex].correct_choice_index,
+      questions: this.state.questions,
+      choices: this.state.questions[this.state.currentIndex].choices,
+    })
   }
 
   render() {
     return (
       <div className="app">
         <Question className="question" QuestionText={this.state.questionText} choice={this.state.choices} onClick={ (clickedAnswer) => this.handleClick(clickedAnswer) }/>
+        <button id="nextQuestion" onClick={ () => this.resetButton()}>Next Question</button>
       </div> 
     );
   }
