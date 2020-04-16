04a142825694cd5568d1b87f831444e9a7c0f065
Simon Huang <huisimonliu@gmail.com>
2020-04-15 18:53:26 -0700

    emptied out the code for new project

--- src/components/Piece.jsx
+++ src/components/Piece.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 // import components
 
-export class EnemyHand extends Component {
-  constructor(props){
+export class Piece extends Component {
+  constructor(props)
+  {
     super(props);
     this.state = {
-      enemyHand:[];
     };
   }
+
   render() {
     return (
         <div>
-           
+
         </div>
     );
   }
 }
 
-export default EnemyHand;
+export default Piece;
