919f6226382fc146a6c836afb0f13bfd3c9dc953
Simon Huang <huisimonliu@gmail.com>
2022-09-10 20:45:55 -0700

    Updates

---
+++ precache-manifest.7b480bb1fbbe80e991f5f23e738e7506.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b7f2819dfff0dba1d74",
+    "url": "/checkers/static/css/main.e5595bce.chunk.css"
+  },
+  {
+    "revision": "2b7f2819dfff0dba1d74",
+    "url": "/checkers/static/js/main.2b7f2819.chunk.js"
+  },
+  {
+    "revision": "c57f9efc4d0b0f863a74",
+    "url": "/checkers/static/js/1.c57f9efc.chunk.js"
+  },
+  {
+    "revision": "d91061b866421ab5ed7b",
+    "url": "/checkers/static/js/runtime~main.d91061b8.js"
+  },
+  {
+    "revision": "824abd86ce4e024f5be1d119ca9732ab",
+    "url": "/checkers/index.html"
+  }
+];
